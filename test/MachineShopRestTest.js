2f4db4df68e32ec190ff6aad2ad519b2eee3ef12
Bruno Silva <bruno_dasilva@epam.com>
2018-03-28 16:04:34 +0200

    New tests.

--- test/MachineShopRestTest.js
+++ test/MachineShopRestTest.js
@@ -1,9 +1,8 @@
-let express = require("../util/express")();
+const express = require("../util/express")();
+const request = require("supertest")(express);
 
-let request = require("supertest")(express);
-
-before(done => {
-    let tool = express.repositories.SchemaManagementTool(express);
+let cleanDatabase = done => {
+    const tool = express.repositories.SchemaManagementTool(express);
 
     tool.clean().then(() => {
         done();
@@ -12,12 +11,25 @@ before(done => {
         console.log(error);
         done();
     });
-});
+};
+
+before(cleanDatabase);
+
+after(cleanDatabase);
 
 describe("MachineShopRestTest", () => {
-    
+    machineShopTest = {
+        "location": {
+            "lat": -21.9448222,
+            "lng": -44.1939177
+        },
+        "_id": "30b55671ea603f0541a37465bf5dc65fa4b3b973",
+        "name" : "Mecânica Dragrão",
+        "vicinity": "348, R. Santos Dumont, 236, Bom Jardim de Minas - MG, 37310-000"
+    };
+
     it("#GET /machine-shop/:id SUCESS", (done) => {
-        request.get("/machine-shop/5ab8e7746c4ef231e056595a")
+        request.get(`/machine-shop/${machineShopTest._id}`)
             .expect("Content-Type", /json/)
             .expect(/null/)
             .expect(200, done);
@@ -42,17 +54,34 @@ describe("MachineShopRestTest", () => {
 
     it("#POST /machine-shop SUCESS", (done) => {
         request.post("/machine-shop")
-            .send({
-                "location": {
-                    "lat": -21.9448222,
-                    "lng": -44.1939177
-                },
-                "name": "Mecânica Dragão",
-                "_id": "30b55671ea603f0541a37465bf5dc65fa4b3b973",
-                "vicinity": "348, R. Santos Dumont, 236, Bom Jardim de Minas - MG, 37310-000"
-            })
+            .send(machineShopTest)
             .expect("Content-Type", /json/)
             .expect(/Machine shop saved/)
+            .expect(200, done);
+    });
+
+    it("#PUT /machine-shop FAIL", (done) => {
+        request.put(`/machine-shop/${machineShopTest._id}`)
+            .send({
+                "name" : ""
+            })
+            .expect("Content-Type", /json/)
+            .expect(/Name is a mandatory field/)
+            .expect(400, done);
+    });
+
+    it("#PUT /machine-shop SUCESS", (done) => {
+        request.put(`/machine-shop/${machineShopTest._id}`)
+            .send(machineShopTest)
+            .expect("Content-Type", /json/)
+            .expect(/Machine shop updated/)
+            .expect(200, done);
+    });
+
+    it("#DELETE /machine-shop SUCESS", (done) => {        
+        request.delete(`/machine-shop/${machineShopTest._id}`)
+            .expect("Content-Type", /json/)
+            .expect(/Machine shop deleted/)
             .expect(200, done);
     });
 });
